625dfe35a73736f7d1a44b7b0a211180328fd91e
Carlos Eduado PC-Trabalho <eduardo201184@hotmail.com>
2024-01-19 17:31:09 -0300

    chamando as variaveis no js

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const id = document.querySelector('#id');
+const mac = document.querySelector('#mac');
+const verificar_Ligado = document.querySelector('#select_Verificar_Ligado');
+const funcionalidade_Olt_Huawei = document.querySelector('#select_Funcionalidade_Olt_Huawei');
+const do_Velocidade_Olt = document.querySelector('#input_Do_Velocidade_Olt');
+const up_Velocidade_Olt = document.querySelector('#input_Up_Velocidade_Olt');
+const velocidade_Cabo_Olt = document.querySelector('#select_Velocidade_Cabo_Olt');
+const do_Velocidade_Wifi_5Ghz = document.querySelector('#input_Do_Velocidade_Wifi_5Ghz');
+const up_Velocidade_Wifi_5Ghz = document.querySelector('#input_Up_Velocidade_Wifi_5Ghz');
+const velocidade_Wifi_5Ghz = document.querySelector('#select_Velocidade_Wifi_5Ghz');
+const verificar_Leds = document.querySelector('#select_Verificar_Leds');
+const modelo_Roteador = document.querySelector('#select_Modelo_Roteador');
+const versao_Roteador = document.querySelector('#Select_Versao_Roteador');
+const testar_PortasLan = document.querySelector('#Select_Testar_PortasLan');
+const velocidade_1gb = document.querySelector('#Select_velocidade_1gb');
+const status_antenas = document.querySelector('#Select_Status_Antenas');
+const grau_Conservacao = document.querySelector('#input_Grau_Conservacao');
+const potencia_24ghz = document.querySelector('#select_Potencia_24ghz');
+const potencia_5ghz = document.querySelector('#select_Potencia_5ghz');
+const ssid = document.querySelector('#select_Ssid');
+const voltagem = document.querySelector('#select_voltagem');
+const po_Verificar_SinalOptico = document.querySelector('#input_Po_Verificar_SinalOptico');
+const sis_Verificar_SinalOptico = document.querySelector('#input_Sis_Verificar_SinalOptico');
+const Verificar_sinalOptico = document.querySelector('#select_Verificar_sinalOptico');
+const aquecimento = document.querySelector('#select_Aquecimento');
+const atualizar_Firmware = document.querySelector('#select_Atualizar_Firmware');
+const ipv6 = document.querySelector('#select_Ipv6');
+const resetarRoteador = document.querySelector('#select_ResetarRoteador');
+const laudo_Final = document.querySelector('#laudo_Final');
+const assinatura = document.querySelector('#assinatura');
+
+
+
+const btn_visualizar = document.querySelector('#btn_visualizar');
+
+btn_visualizar.addEventListener('click', ()=> {
+
+    console.log(id.value);
+})
